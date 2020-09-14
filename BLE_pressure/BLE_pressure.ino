30d178d8ad0636952995cfc074fa842126a34d54
Helmut Strey <Helmut.Strey@gmail.com>
2020-09-14 15:53:47 -0400

    final sensor sketch

--- BLE_pressure/BLE_pressure.ino
+++ BLE_pressure/BLE_pressure.ino
@@ -16,19 +16,27 @@
 #include <SPI.h>
 #include <BLEPeripheral.h>
 #include <BLEUtil.h>
+#include <string.h> 
 
 BLEPeripheral gaitPeripheral = BLEPeripheral();
 TimerClass timer(nrf_timer_num, cc_channel_num);
 
 int taking_data = false;
-int sensorValue = 0;  // variable to store the value coming from the sensor
+unsigned short sensorValue = 0;  // variable to store the value coming from the sensor
+int intervalmicsec = 300000;
+int i,factor;
+int counter = 0;
+unsigned char data[20];
+char* dataPointer;
+char* valuePointer;
+
 
 // create remote services
 BLEService GaitService = BLEService("573bc6051c3c4467b4aa44e0c6c6b410");
 
 // create remote characteristics
 BLECharacteristic pressureCharacteristic = BLECharacteristic("5a8ac39c0ce142128b46d589ba126ce2", BLERead | BLENotify ,20);
-BLECharacteristic periodCharacteristic = BLECharacteristic("f8bc0798e4474625959fce6970b70ed1", BLEWrite,4);
+BLECharacteristic periodCharacteristic = BLECharacteristic("f8bc0798e4474625959fce6970b70ed1", BLERead | BLEWrite,4);
 
 
 void setup() {
@@ -36,8 +44,17 @@ void setup() {
   digitalWrite(OUT_PIN, 1);
   pinMode(anio2, INPUT_PULLUP);
   
+  valuePointer = (char*)&sensorValue;
+  dataPointer = (char*)&data;
+  
+
   // put your setup code here, to run once:
   Serial.begin(115200);
+  delay(1000);
+  
+  // trying to find out what the size of int is for RF52
+  Serial.print("sizeof int: ");
+  Serial.println(sizeof(intervalmicsec));
   
   gaitPeripheral.setLocalName("Pressure-Sensor1");
 
@@ -88,15 +105,22 @@ void blePeripheralDisconnectHandler(BLECentral& central) {
 
 void pressureCharacteristicValueWrittenHandle(BLECentral& central, BLECharacteristic& characteristic) {
   Serial.print(F("Written characteristic value: "));
-
-  BLEUtil::printBuffer(characteristic.value(), characteristic.valueLength());
+  BLEUtil::printBuffer(characteristic.value(),characteristic.valueLength());
+  intervalmicsec = 0;
+  factor = 1;
+  for (i=3;i>=0;--i) {
+    intervalmicsec += int(characteristic.value()[i])*factor;
+    factor = factor*256;
+  }
+  Serial.print(F("after conversion: "));
+  Serial.println(intervalmicsec);
 }
 
 void pressureCharacteristicSubscribedHandle(BLECentral& central, BLECharacteristic& characteristic) {
   Serial.print(F("Subscribed to characteristic value: "));
   taking_data = true;
   BLEUtil::printBuffer(characteristic.value(), characteristic.valueLength());
-  timer.attachInterrupt(&Timer_callback, 300000); // microseconds
+  timer.attachInterrupt(&Timer_callback, intervalmicsec); // microseconds
   taking_data = true;
   digitalWrite(OUT_PIN, 0);
 }
@@ -110,8 +134,20 @@ void pressureCharacteristicUnSubscribedHandle(BLECentral& central, BLECharacteri
 
 void Timer_callback() {
     if (taking_data) {
-      timer.attachInterrupt(&Timer_callback, 300000); // microseconds
+      timer.attachInterrupt(&Timer_callback, intervalmicsec); // microseconds
       sensorValue = analogRead(anio2);
+      // copy value into char buffer
+      for(int i = 0; i < 2; i++) {
+        dataPointer[i] = valuePointer[i];
+      }
+      dataPointer +=2;
+      counter +=1;
+      if (counter>9) {
+        pressureCharacteristic.setValue(data,20);
+        counter = 0;
+        dataPointer = (char*)&data;
+        Serial.print("sent data package...");
+      }
       Serial.print(F("Measuring... "));
       Serial.println(sensorValue);
    }
