33dba551d98ca184b6f91582a41c6d7ae1515f9e
Helmut Strey <Helmut.Strey@gmail.com>
2020-10-26 19:30:47 -0400

    end of class 10/26/20

--- django/mb/posts/models.py
+++ django/mb/posts/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    title = models.CharField(max_length=50, default="No Title")
+    author = models.ForeignKey('auth.User', default=1,
+    on_delete=models.CASCADE,
+    )
     text = models.TextField()
 
     def __str__(self):
